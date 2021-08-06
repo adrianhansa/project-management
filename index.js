2bc7ab8d3594fda4bdc5edb2c61b3e85dfadd0ab
Adrian N. Hansa <adrian.hansa@yahoo.com>
2021-08-06 19:23:53 +0100

    installed cors & morgan, setting up the server and connetion to mongodb

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv/config");
+const morgan = require("morgan");
+const cors = require("cors");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(express.json());
+app.use(morgan("tiny"));
+app.use(cors({ origin: ["http://localhost:3000"], httpOnly: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+mongoose
+  .connect(process.env.DB_CONNECTION, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("Server connected to mongodb.");
+    app.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+    });
+  })
+  .catch((error) => console.log(error));
